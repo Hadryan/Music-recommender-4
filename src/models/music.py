76c19f75a60196b0c69012386ff89e9e878b117f
Yuzo <yuzonakai@gmail.com>
2020-11-16 15:41:58 +0000

    Fix some bugs.

--- src/models/music.py
+++ src/models/music.py
@@ -1,7 +1,7 @@
 class Music:
     def __init__(self, params):
-        self.name = params.get('name')
-        self.artist = params.get('artist')
+        self.name = params.get('track_name')
+        self.artist = params.get('artist_name')
         self.release_date = params.get('release_date')
         self.genres = params.get('genres') or []
 
