0707f022acf9b23408f6e21fa1101126e86b24a7
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2020-11-16 17:37:27 +0000

    fix: birth_date None on persons create

--- src/neo4j/create_persons.py
+++ src/neo4j/create_persons.py
@@ -3,6 +3,7 @@ from src.models.person import Person
 from ..neo4j.connection_provider import ConnectionProvider
 from faker import Faker
 from random import randrange
+from datetime import datetime
 
 
 class PersonCreator:
@@ -13,7 +14,7 @@ class PersonCreator:
         for _ in range(quantity):
             params = {}
             params['name'] = faker.name()
-            params['age'] = randrange(15, 70)
+            params['birth_date'] = datetime(randrange(1950, 2000), randrange(1,12),randrange(1,28)).strftime("%Y")
             person = Person(params)
             ConnectionProvider.create_node(person)
             print('.')
