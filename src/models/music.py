8e85567797f4ee6e5ca49923aa44b7e3c18d38d3
Yuzo <yuzonakai@gmail.com>
2020-11-16 15:52:37 +0000

    c

--- src/models/music.py
+++ src/models/music.py
@@ -1,7 +1,7 @@
 class Music:
     def __init__(self, params):
-        self.name = params.get('track_name')
-        self.artist = params.get('artist_name')
+        self.name = params.get('name')
+        self.artist = params.get('artist')
         self.release_date = params.get('release_date')
         self.genres = params.get('genres') or []
 
--- src/neo4j/create_musics.py
+++ src/neo4j/create_musics.py
@@ -7,20 +7,18 @@ import pandas as pd
 class MusicCreator:
     @staticmethod
     def run():
-        """
-        w
-        """
         ConnectionProvider.cleanup_nodes(Music)
         tracks = pd.read_csv('./data/sampled_tracks.csv', sep=',')
         for track in tracks.iterrows():
             params = {}
-            params['track_name'] = track[1]['track_name']
-            params['artist_name'] = track[1]['artist_name']
+            params['name'] = track[1]['track_name']
+            params['artist'] = track[1]['artist_name']
             params['release_date'] = track[1]['release_date']
             genres = track[1]['genres'][1:len(track[1]['genres'])-1]
             params['genres'] = genres.replace('\'', '').split(',')
             music = Music(params)
             ConnectionProvider.create_node(music)
+            print('.')
 
 
 if __name__ == "__main__":
