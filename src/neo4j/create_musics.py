b7f3e27279246a825706a8b846b5cce17a5d8312
Eduardo Barbosa de Oliveira <edu.bar.oliveira@gmail.com>
2020-11-16 21:56:23 +0000

    refactory: changed columns names on csv

--- src/neo4j/create_musics.py
+++ src/neo4j/create_musics.py
@@ -11,8 +11,8 @@ class MusicCreator:
         tracks = pd.read_csv('./data/sampled_tracks.csv', sep=',')
         for track in tracks.iterrows():
             params = {}
-            params['name'] = track[1]['track_name']
-            params['artist'] = track[1]['artist_name']
+            params['name'] = track[1]['name']
+            params['artist'] = track[1]['name']
             params['release_date'] = track[1]['release_date']
             genres = track[1]['genres'][1:len(track[1]['genres'])-1]
             params['genres'] = genres.replace('\'', '').split(',')
